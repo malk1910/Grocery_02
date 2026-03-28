ba5731f803be249e3a1d2907139082f8639d86f7
malk1910 <engmalkayman2005@gmail.com>
2026-03-28 22:52:17 +0200

    products section

--- script.js
+++ script.js
@@ -7,3 +7,38 @@ burger.addEventListener("click", () => {
     navLinks.classList.toggle("active");
     burger.classList.toggle("toggle");
 });
+
+
+// Automatically Slider
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 3,
+      spaceBetween: 20,
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        992: {
+          slidesPerView: 3,
+        }
+      }
+    });
+  });
